741e309f654da749d1d63de84f113cc735fb0b8d
Stanley <stanley92@hotmail.com>
2017-02-09 16:24:14 +0800

    Implemented PwdApplication

--- src/sg/edu/nus/comp/cs4218/impl/app/PwdApplication.java
+++ src/sg/edu/nus/comp/cs4218/impl/app/PwdApplication.java
@@ -4,6 +4,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 import sg.edu.nus.comp.cs4218.Application;
+import sg.edu.nus.comp.cs4218.Environment;
 import sg.edu.nus.comp.cs4218.exception.AbstractApplicationException;
 
 /**
@@ -25,7 +26,11 @@ public class PwdApplication implements Application{
 	@Override
 	public void run(String[] args, InputStream stdin, OutputStream stdout) throws AbstractApplicationException {
 		// TODO Auto-generated method stub
-		
+		if (args.length != 1) {
+			System.out.println("Too many arguments. Pwd do not accept any arguments.");
+		} else {
+			String workingDir = Environment.currentDirectory;
+			System.out.println(workingDir);		
+		}
 	}
-
 }
