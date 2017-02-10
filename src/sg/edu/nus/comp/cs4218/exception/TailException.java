e2cba81085b2d756e15cde812d4a822c00e9a644
Stanley <stanley92@hotmail.com>
2017-02-10 11:41:35 +0800

    Implemented Tail Exception

---
+++ src/sg/edu/nus/comp/cs4218/exception/TailException.java
@@ -0,0 +1,8 @@
+package sg.edu.nus.comp.cs4218.exception;
+
+public class TailException extends AbstractApplicationException {
+
+	public TailException(String message) {
+		super("Tail: " + message);
+	}
+}
