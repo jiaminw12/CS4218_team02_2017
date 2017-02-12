a7fc20e4579bfbc918a44cbbcc996f784192957f
Stanley <stanley92@hotmail.com>
2017-02-12 11:03:44 +0800

    Added Basic Functionality test files

---
+++ test/sg/edu/nus/comp/cs4218/impl/app/EchoApplicationTest.java
@@ -0,0 +1,14 @@
+package sg.edu.nus.comp.cs4218.impl.app;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EchoApplicationTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
