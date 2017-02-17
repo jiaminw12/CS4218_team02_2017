878b501980a660b9b244e2bf63c306429bb0bf1e
Jia Min <jiaminw@ymail.com>
2017-02-18 03:10:37 +0800

    Updated IORedirectionTest

--- test-ef1/sg/edu/nus/comp/cs4218/impl/ef1/IORedirectionTest.java
+++ test-ef1/sg/edu/nus/comp/cs4218/impl/ef1/IORedirectionTest.java
@@ -3,14 +3,15 @@ package sg.edu.nus.comp.cs4218.impl.ef1;
 import static org.junit.Assert.assertEquals;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Test;
 
-import sg.edu.nus.comp.cs4218.Environment;
 import sg.edu.nus.comp.cs4218.exception.AbstractApplicationException;
 import sg.edu.nus.comp.cs4218.exception.ShellException;
 import sg.edu.nus.comp.cs4218.impl.ShellImpl;
@@ -21,96 +22,99 @@ public class IORedirectionTest {
 	private static ByteArrayOutputStream stdout;
 	String[] args;
 
-	static String readFile(String path) throws IOException {
-		byte[] byteArr = Files.readAllBytes(Paths.get(System
-				.getProperty("user.dir") + "/test-files-ef2/" + path));
+	private String readFile(String path) throws IOException {
+		byte[] byteArr = Files
+				.readAllBytes(Paths.get(System.getProperty("user.dir") + path));
 		return new String(byteArr);
 	}
 
 	@Before
 	public void setUp() {
-		Environment.currentDirectory = System.getProperty("user.dir")
-				+ "/test-files-ef2/";
 		shell = new ShellImpl();
+		stdout = new ByteArrayOutputStream();
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		File file = new File("a.txt");
+		file.setWritable(true);
+		file.delete();
+
+		file = new File("b.txt");
+		file.setWritable(true);
+		file.delete();
+	}
+
+	@Test(expected = ShellException.class)
+	public void testIllegalIORedirectionOneLine()
+			throws AbstractApplicationException, ShellException {
+		String cmdLine = "echo TEST > a.txt > b.txt";
+		shell.parseAndEvaluate(cmdLine, stdout);
+	}
+	
+	@Test(expected = ShellException.class)
+	public void testIllegalIORedirectionMultipleLines() throws Exception {
+		String cmdLine = "echo TEST > a.txt";
+		shell.parseAndEvaluate(cmdLine, stdout);
+
+		cmdLine = "echo TEST > b.txt";
+		shell.parseAndEvaluate(cmdLine, stdout);
+
+		cmdLine = "grep TEST < a.txt < b.txt";
+		shell.parseAndEvaluate(cmdLine, stdout);
+	}
+	
+	@Test(expected = ShellException.class)
+	public void testIORedirectionWithEmtpyFile() throws Exception {
+		String cmdLine = "grep TEST < ";
+		shell.parseAndEvaluate(cmdLine, stdout);
+	}
+
+	@Test(expected = ShellException.class)
+	public void testIORedirectionWithIllegalFile() throws Exception {
+		String cmdLine = "grep TEST < nofile.txt";
+		shell.parseAndEvaluate(cmdLine, stdout);
+	}
+	
+	@Test(expected = ShellException.class)
+	public void testIORedirectionWithIllegalFormat() throws Exception {
+		String cmdLine = "grep TEST < > nofile.txt";
+		shell.parseAndEvaluate(cmdLine, stdout);
 	}
 
 	@Test
-	public void testIORedirectionPositve1() throws Exception {
-		String cmdLine = "echo ABC > a.txt";
-		stdout = new ByteArrayOutputStream();
+	public void testIORedirectionWithEcho() throws Exception {
+		String cmdLine = "echo TEST > a.txt";
 		shell.parseAndEvaluate(cmdLine, stdout);
-
-		String expected = "ABC" + System.lineSeparator();
-
+		String expected = "TEST" + System.lineSeparator();
 		assertEquals(expected, readFile("a.txt"));
 	}
 
 	@Test
-	public void testIORedirectionPositve2() throws Exception {
-		String cmdLine1 = "echo ABC > a.txt";
-		stdout = new ByteArrayOutputStream();
+	public void testIORedirectionWithEchoGrep() throws Exception {
+		String cmdLine1 = "echo TEST > a.txt";
 		shell.parseAndEvaluate(cmdLine1, stdout);
 
-		String cmdLine2 = "grep ABC < a.txt";
+		String cmdLine2 = "grep TEST < a.txt";
 		stdout = new ByteArrayOutputStream();
 		shell.parseAndEvaluate(cmdLine2, stdout);
 
-		String expected = "ABC" + System.lineSeparator();
-
+		String expected = "TEST" + System.lineSeparator();
 		assertEquals(expected, stdout.toString());
 	}
 
 	@Test
-	public void testIORedirectionPositve3() throws Exception {
-		String cmdLine1 = "echo ABC > a.txt";
-		stdout = new ByteArrayOutputStream();
+	public void testIORedirectionWithLeftAndRight() throws Exception {
+		String cmdLine1 = "echo TEST > a.txt";
 		shell.parseAndEvaluate(cmdLine1, stdout);
 
-		String cmdLine2 = "grep ABC < a.txt > b.txt";
+		String cmdLine2 = "grep TEST < a.txt > b.txt";
 		stdout = new ByteArrayOutputStream();
 		shell.parseAndEvaluate(cmdLine2, stdout);
 
-		String expected = "ABC"+System.lineSeparator();
+		String expected = "TEST" + System.lineSeparator();
 
 		assertEquals(expected, readFile("b.txt"));
 	}
 
-	// The following test check the following requirement:
-	// "If several files are specified for input redirection or output
-	// redirection, throw an exception."
-	@Test(expected = ShellException.class)
-	public void testIORedirectionNegative1()
-			throws AbstractApplicationException, ShellException {
-		String cmdLine = "echo ABC > a.txt > b.txt";
-		stdout = new ByteArrayOutputStream();
-		shell.parseAndEvaluate(cmdLine, stdout);
-	}
-
-	// The following test check the following requirement:
-	// "If several files are specified for input redirection or output
-	// redirection, throw an exception."
-	@Test(expected = ShellException.class)
-	public void testIORedirectionNegative2() throws Exception {
-		String cmdLine1 = "echo ABC > a.txt";
-		stdout = new ByteArrayOutputStream();
-		shell.parseAndEvaluate(cmdLine1, stdout);
-
-		String cmdLine2 = "echo ABC > b.txt";
-		stdout = new ByteArrayOutputStream();
-		shell.parseAndEvaluate(cmdLine2, stdout);
-
-		String cmdLine3 = "grep ABC < a.txt < b.txt";
-		stdout = new ByteArrayOutputStream();
-		shell.parseAndEvaluate(cmdLine3, stdout);
-	}
-
-	// The following test check the following requirement:
-	// "If the file specified for input redirection does not exist, throw an
-	// exception."
-	@Test(expected = ShellException.class)
-	public void testIORedirectionNegative3() throws Exception {
-		String cmdLine = "grep ABC < nofile.txt";
-		stdout = new ByteArrayOutputStream();
-		shell.parseAndEvaluate(cmdLine, stdout);
-	}
 }
