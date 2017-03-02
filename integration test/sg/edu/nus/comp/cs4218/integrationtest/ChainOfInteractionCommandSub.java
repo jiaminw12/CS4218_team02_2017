4ea623b1a8cfcbd13410a50217ad9e1631ef9f54
Jia Min <jiaminw@ymail.com>
2017-03-03 01:18:19 +0800

    Added Integration test folder

---
+++ integration test/sg/edu/nus/comp/cs4218/integrationtest/ChainOfInteractionCommandSub.java
@@ -0,0 +1,23 @@
+package sg.edu.nus.comp.cs4218.integrationtest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ChainOfInteractionCommandSub {
+
+	// Structural integration
+	// Test integration with at least one other application
+	// Test integration of EF1 and 2 with basic functionality
+
+	// Functional integration
+	// Chains of interactions: cat `head ..` | sort .. (use command
+	// substitution, piping, multiple tools,etc.)
+	// Negative scenarios where one application returns an error
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ integration test/sg/edu/nus/comp/cs4218/integrationtest/ChainOfInteractionPiping.java
@@ -0,0 +1,14 @@
+package sg.edu.nus.comp.cs4218.integrationtest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ChainOfInteractionPiping {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
