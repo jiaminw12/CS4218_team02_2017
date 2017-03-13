5039f7ea7cd8ff50377abf2660f904dfca9d013d
Jia Min <jiaminw@ymail.com>
2017-03-14 01:44:00 +0800

    Updated CommandSubTest

--- test/sg/edu/nus/comp/cs4218/impl/cmd/CommandSubTest.java
+++ test/sg/edu/nus/comp/cs4218/impl/cmd/CommandSubTest.java
@@ -28,150 +28,154 @@ public class CommandSubTest {
 		shellImpl = new ShellImpl();
 	}
 
-	@Test(expected = ShellException.class)
+	@Test
 	public void testIllegalApplicationName()
 			throws ShellException, AbstractApplicationException {
 		String cmdLine = "echo `echa token1 `";
-		outputStream = new ByteArrayOutputStream();
-		shellImpl.parseAndEvaluate(cmdLine, outputStream);
+		String result = shellImpl.performCommandSubstitutionWithException(cmdLine);
+		assertEquals("shell: echa: Invalid app.", result.trim());
 	}
 
-	@Test(expected = ShellException.class)
+	@Test
 	public void testIllegalNumOfBackQuoteAtBack()
 			throws ShellException, AbstractApplicationException {
 		String cmdLine = "echo ` token1 ```";
-		outputStream = new ByteArrayOutputStream();
-		shellImpl.parseAndEvaluate(cmdLine, outputStream);
+		String result = shellImpl.performCommandSubstitutionWithException(cmdLine);
+		assertEquals("shell: Invalid syntax encountered.", result.trim());
 	}
 
-	@Test(expected = ShellException.class)
+	@Test
 	public void testIllegalNumOfBackQuoteAtStart()
 			throws ShellException, AbstractApplicationException {
 		String cmdLine = "echo ``` token1 `";
-		outputStream = new ByteArrayOutputStream();
-		shellImpl.parseAndEvaluate(cmdLine, outputStream);
+		String result = shellImpl.performCommandSubstitutionWithException(cmdLine);
+		assertEquals("shell: Invalid syntax encountered.", result.trim());
 	}
 
-	@Test(expected = ShellException.class)
+	@Test
 	public void testIllegalCommand()
 			throws AbstractApplicationException, ShellException {
 		String cmdLine = "echo `cat farfarway.txt |`";
-		outputStream = new ByteArrayOutputStream();
-		shellImpl.parseAndEvaluate(cmdLine, outputStream);
+		String result = shellImpl.performCommandSubstitutionWithException(cmdLine);
+		assertEquals("shell: Invalid pipe operators", result.trim());
 	}
 
-	@Test(expected = ShellException.class)
+	@Test
 	public void testInvalidArg()
 			throws ShellException, AbstractApplicationException, IOException {
 		String cmdLine = "echo `hahaha.... echo` echo";
-		outputStream = new ByteArrayOutputStream();
-		shellImpl.parseAndEvaluate(cmdLine, outputStream);
+		String result = shellImpl.performCommandSubstitutionWithException(cmdLine);
+		assertEquals("shell: hahaha....: Invalid app.", result.trim());
 	}
 
-	@Test(expected = ShellException.class)
+	@Test
 	public void testBQContainBQ()
 			throws ShellException, AbstractApplicationException, IOException {
 		String cmdLine = "echo `echo `echo testing ing ....``";
-		outputStream = new ByteArrayOutputStream();
-		shellImpl.parseAndEvaluate(cmdLine, outputStream);
+		String result = shellImpl.performCommandSubstitutionWithException(cmdLine);
+		assertEquals("shell: Invalid syntax encountered.", result.trim());
 	}
 
 	@Test
 	public void testEchoWithSemicolon() throws Exception {
 		String cmdLine = "echo `echo ABC; echo DEF`";
-		outputStream = new ByteArrayOutputStream();
-		shellImpl.parseAndEvaluate(cmdLine, outputStream);
-		String actualResult = new String(outputStream.toByteArray()).trim();
-		String expectedResult = "ABCDEF" + System.lineSeparator();
-		assertEquals(expectedResult.trim(), actualResult);
+		String result = shellImpl.performCommandSubstitution(cmdLine);
+		assertEquals("ABCDEF", result.trim());
 	}
 
 	@Test
 	public void testCommandSub() throws Exception {
 		String cmdLine = "echo `cat farfaraway.txt`";
-		outputStream = new ByteArrayOutputStream();
-		shellImpl.parseAndEvaluate(cmdLine, outputStream);
-		String actualResult = new String(outputStream.toByteArray()).trim();
+		String result = shellImpl.performCommandSubstitution(cmdLine);
 		assertEquals(
-				"Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.A small river named Duden flows by their place and supplies it with the necessary regelialia.It is a paradisematic country, in which roasted parts of sentences fly into your mouth.At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint.",
-				actualResult);
+				"Far far away, behind the word mountains, far from the countries Vokalia "
+						+ "and Consonantia, there live the blind texts.Separated they live in "
+						+ "Bookmarksgrove right at the coast of the Semantics, a large language "
+						+ "ocean.A small river named Duden flows by their place and supplies it "
+						+ "with the necessary regelialia.It is a paradisematic country, in which "
+						+ "roasted parts of sentences fly into your mouth.At vero eos et accusamus "
+						+ "et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum "
+						+ "deleniti atque corrupti quos dolores et quas molestias excepturi sint.",
+				result.trim());
 	}
 
 	@Test
 	public void testInputSingleQuote()
 			throws IOException, AbstractApplicationException, ShellException {
 		String cmdLine = "echo '`cat farfaraway.txt`' ";
-		outputStream = new ByteArrayOutputStream();
-		shellImpl.parseAndEvaluate(cmdLine, outputStream);
-		String actualResult = new String(outputStream.toByteArray()).trim();
-		assertEquals("`cat farfaraway.txt`", actualResult);
+		String result = shellImpl.performCommandSubstitution(cmdLine);
+		assertEquals("`cat farfaraway.txt`", result.trim());
 	}
 
 	@Test
 	public void testOneCommandSubWithNewLine()
 			throws AbstractApplicationException, ShellException {
 		String cmdLine = "echo `cat " + System.lineSeparator() + "slicing.txt`";
-		outputStream = new ByteArrayOutputStream();
-		shellImpl.parseAndEvaluate(cmdLine, outputStream);
-		String actualResult = new String(outputStream.toByteArray()).trim();
+		String result = shellImpl.performCommandSubstitution(cmdLine);
 		assertEquals(
-				"Program slicing can be used in debugging to locate source of errors more easily.",
-				actualResult);
+				"Program slicing can be used in debugging to locate source of "
+						+ "errors more easily.",
+				result.trim());
 	}
 
 	@Test
 	public void testOneCommandSubWithDoubleQuote()
 			throws AbstractApplicationException, ShellException {
 		String cmdLine = "echo \"`echo popeye here`\"";
-		outputStream = new ByteArrayOutputStream();
-		shellImpl.parseAndEvaluate(cmdLine, outputStream);
-		String actualResult = new String(outputStream.toByteArray()).trim();
-		assertEquals("popeye here", actualResult);
+		String result = shellImpl.performCommandSubstitution(cmdLine);
+		assertEquals("popeye here", result.trim());
 	}
 
 	@Test
 	public void testOneCommandSub()
 			throws AbstractApplicationException, ShellException {
 		String cmdLine = "echo `wc -l slicing.txt`";
-		outputStream = new ByteArrayOutputStream();
-		shellImpl.parseAndEvaluate(cmdLine, outputStream);
-		String actualResult = new String(outputStream.toByteArray()).trim();
-		assertEquals("1", actualResult);
+		String result = shellImpl.performCommandSubstitution(cmdLine);
+		assertEquals("0 slicing.txt", result.trim());
 	}
 
 	@Test
 	public void testCommandSubWithPipeHeadWithNoOptions() throws Exception {
 		String cmdLine = "echo `cat farfaraway.txt | head`";
-		outputStream = new ByteArrayOutputStream();
-		shellImpl.parseAndEvaluate(cmdLine, outputStream);
-		String actualResult = new String(outputStream.toByteArray()).trim();
+		String result = shellImpl.performCommandSubstitution(cmdLine);
 		assertEquals(
-				"Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.A small river named Duden flows by their place and supplies it with the necessary regelialia.It is a paradisematic country, in which roasted parts of sentences fly into your mouth.At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint.",
-				actualResult);
+				"Far far away, behind the word mountains, far from the countries "
+						+ "Vokalia and Consonantia, there live the blind texts.Separated "
+						+ "they live in Bookmarksgrove right at the coast of the Semantics, "
+						+ "a large language ocean.A small river named Duden flows by their place "
+						+ "and supplies it with the necessary regelialia.It is a paradisematic "
+						+ "country, in which roasted parts of sentences fly into your mouth.At vero"
+						+ " eos et accusamus et iusto odio dignissimos ducimus qui blanditiis "
+						+ "praesentium voluptatum deleniti atque corrupti quos dolores et quas "
+						+ "molestias excepturi sint.",
+				result.trim());
 	}
 
 	@Test
 	public void testOneCommandWithPipeHead()
 			throws AbstractApplicationException, ShellException {
 		String cmdLine = "echo `cat farfaraway.txt | head -n 1`";
-		outputStream = new ByteArrayOutputStream();
-		shellImpl.parseAndEvaluate(cmdLine, outputStream);
-		String actualResult = new String(outputStream.toByteArray()).trim();
+		String result = shellImpl.performCommandSubstitution(cmdLine);
 		assertEquals(
-				"Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
-				actualResult);
+				"Far far away, behind the word mountains, far from the countries "
+				+ "Vokalia and Consonantia, there live the blind texts.",
+				result.trim());
 	}
 
 	@Test
 	public void testOneCommandWithPipeTail()
 			throws AbstractApplicationException, ShellException {
 		String cmdLine = "echo `cat farfaraway.txt | tail -n 5`";
-		outputStream = new ByteArrayOutputStream();
-		shellImpl.parseAndEvaluate(cmdLine, outputStream);
-		String actualResult = new String(outputStream.toByteArray()).trim();
-		assertEquals(
-				"Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.A small river named Duden flows by their place and supplies it with the necessary regelialia.It is a paradisematic country, in which roasted parts of sentences fly into your mouth.At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint.",
-				actualResult);
+		String result = shellImpl.performCommandSubstitution(cmdLine);
+		assertEquals("Far far away, behind the word mountains, far from the countries "
+				+ "Vokalia and Consonantia, there live the blind texts.Separated "
+				+ "they live in Bookmarksgrove right at the coast of the Semantics, "
+				+ "a large language ocean.A small river named Duden flows by their place "
+				+ "and supplies it with the necessary regelialia.It is a paradisematic "
+				+ "country, in which roasted parts of sentences fly into your mouth.At vero"
+				+ " eos et accusamus et iusto odio dignissimos ducimus qui blanditiis "
+				+ "praesentium voluptatum deleniti atque corrupti quos dolores et quas "
+				+ "molestias excepturi sint.", result.trim());
 	}
 
 	@Test
@@ -179,13 +183,13 @@ public class CommandSubTest {
 			throws AbstractApplicationException, ShellException {
 		String originalPath = Environment.currentDirectory;
 		String cmdLine = "echo `cat slicing.txt; cd test`";
-		outputStream = new ByteArrayOutputStream();
-		shellImpl.parseAndEvaluate(cmdLine, outputStream);
-		String actualResult = new String(outputStream.toByteArray()).trim();
+		String result = shellImpl.performCommandSubstitution(cmdLine);
 		assertEquals(
-				"Program slicing can be used in debugging to locate source of errors more easily.",
-				actualResult);
-		assertEquals(originalPath + File.separatorChar + "test", Environment.currentDirectory);
+				"Program slicing can be used in debugging to locate source of "
+						+ "errors more easily.",
+				result.trim());
+		assertEquals(originalPath + File.separatorChar + "test",
+				Environment.currentDirectory);
 		Environment.currentDirectory = originalPath;
 	}
 
@@ -193,48 +197,40 @@ public class CommandSubTest {
 	public void testOneCommandWithSemicolonCatPipe()
 			throws AbstractApplicationException, ShellException {
 		String cmdLine = "echo `cat slicing.txt` ; echo `cat slicing.txt` | echo";
-		outputStream = new ByteArrayOutputStream();
-		shellImpl.parseAndEvaluate(cmdLine, outputStream);
-		String actualResult = new String(outputStream.toByteArray()).trim();
+		String result = shellImpl.performCommandSubstitution(cmdLine);
 		assertEquals(
 				"Program slicing can be used in debugging to locate source of errors more easily.",
-				actualResult);
+				result.trim());
 	}
 
 	@Test
 	public void testMultipleCommandSub()
 			throws AbstractApplicationException, ShellException {
 		String cmdLine = "echo `cat slicing.txt` ; echo `wc -l farfaraway.txt`";
-		outputStream = new ByteArrayOutputStream();
-		shellImpl.parseAndEvaluate(cmdLine, outputStream);
-		String actualResult = new String(outputStream.toByteArray()).trim();
+		String result = shellImpl.performCommandSubstitution(cmdLine);
 		assertEquals(
 				"Program slicing can be used in debugging to locate source of errors more easily."
-						+ System.lineSeparator() + "5",
-				actualResult);
+						+ System.lineSeparator() + "       4 farfaraway.txt",
+				result.trim());
 	}
 
 	@Test
 	public void testMultiplePipeSub()
 			throws AbstractApplicationException, ShellException {
 		String cmdLine = "echo `cat slicing.txt | tail -n 1` | cat";
-		outputStream = new ByteArrayOutputStream();
-		shellImpl.parseAndEvaluate(cmdLine, outputStream);
-		String actualResult = new String(outputStream.toByteArray()).trim();
+		String result = shellImpl.performCommandSubstitution(cmdLine);
 		assertEquals(
 				"Program slicing can be used in debugging to locate source of errors more easily.",
-				actualResult);
+				result.trim());
 	}
 
 	@Test
 	public void testMultipleSemicolonSub()
 			throws AbstractApplicationException, ShellException {
 		String cmdLine = "echo `echo Showing contents of text1.txt cat text1.txt` ; echo yea";
-		outputStream = new ByteArrayOutputStream();
-		shellImpl.parseAndEvaluate(cmdLine, outputStream);
-		String actualResult = new String(outputStream.toByteArray()).trim();
+		String result = shellImpl.performCommandSubstitution(cmdLine);
 		assertEquals("Showing contents of text1.txt cat text1.txt"
-				+ System.lineSeparator() + "yea", actualResult);
+				+ System.lineSeparator() + "yea", result.trim());
 	}
 
 	@After
