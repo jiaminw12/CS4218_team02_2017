086b829262d62944f95c663dc6bb433e75660cb6
Ong Stanley Wei Wen <stanley92@hotmail.com>
2017-03-14 15:21:44 +0800

    Revert "Implemented grepFromOneFile Method in GrepApplication"
    This reverts commit 248671f0f5c6a0af25f822c2e931097ef1a1549c.

--- src/sg/edu/nus/comp/cs4218/impl/app/GrepApplication.java
+++ src/sg/edu/nus/comp/cs4218/impl/app/GrepApplication.java
@@ -1,24 +1,12 @@
 package sg.edu.nus.comp.cs4218.impl.app;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import sg.edu.nus.comp.cs4218.Application;
 import sg.edu.nus.comp.cs4218.app.Grep;
 import sg.edu.nus.comp.cs4218.exception.GrepException;
-import sg.edu.nus.comp.cs4218.exception.HeadException;
 
 /**
  * 
@@ -71,87 +59,13 @@ public class GrepApplication implements Application, Grep {
 	@Override
 	public String grepFromOneFile(String args) {
 		// TODO Auto-generated method stub
-		String fileContent = "";
-		String [] stringArray= args.split(" ");
-		Pattern pattern = Pattern.compile(stringArray[0]);
-		BufferedReader reader;
-		int lineCount = 0;
-		try {
-			File file = new File(stringArray[1]);
-			reader = new BufferedReader(new FileReader(file));
-			String line = reader.readLine();
-			Matcher matcher;
-			while (line!=null) {
-				matcher = pattern.matcher(line);
-				if (matcher.find()) {
-					if (lineCount > 0) {
-						fileContent = fileContent + System.lineSeparator() + line;
-					} else {
-						fileContent = fileContent + line;
-					}	
-					lineCount++;
-				}
-				line = reader.readLine();
-			}
-			reader.close();
-		} catch (IOException e) {
-		}
-		if (lineCount == 0) {
-			fileContent = "Pattern Not Found In File!";
-		}
-		
-		return fileContent;
+		return null;
 	}
 
 	@Override
 	public String grepFromMultipleFiles(String args) {
 		// TODO Auto-generated method stub
-		ArrayList <File> listOfTextFiles = new ArrayList <File>();
-		String fileContent = "";
-		String [] stringArray= args.split(" ");
-		Pattern pattern = Pattern.compile(stringArray[0]);
-		File file;
-		BufferedReader reader;
-		int lineCount = 0;
-		
-		for (int i=1; i<stringArray.length; i++) {
-			try {
-				if (checkValidFile(new File(stringArray[i]))) {
-					file = new File(stringArray[i]);
-					listOfTextFiles.add(file);
-				}
-			} catch (GrepException e) {
-				
-			}
-//			System.out.println(stringArray[i]);
-		}
-		
-		for (int j=0; j<listOfTextFiles.size(); j++) {
-			try {
-				reader = new BufferedReader(new FileReader(listOfTextFiles.get(j)));
-				String line = reader.readLine();
-				Matcher matcher;
-				while (line!=null) {
-					matcher = pattern.matcher(line);
-					if (matcher.find()) {
-						if (lineCount > 0) {
-							fileContent = fileContent + System.lineSeparator() + line;
-						} else {
-							fileContent = fileContent + line;
-						}	
-						lineCount++;
-					}
-					line = reader.readLine();
-				}
-				reader.close();
-			} catch (IOException e) {
-			}
-		}
-		if (lineCount == 0) {
-			fileContent = "Pattern Not Found In File!";
-		}
-		
-		return fileContent;
+		return null;
 	}
 
 	@Override
