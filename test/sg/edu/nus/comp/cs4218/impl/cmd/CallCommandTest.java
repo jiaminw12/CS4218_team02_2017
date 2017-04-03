aeb2b130f900db6784cdc79308b4453ece3c6081
Jia Min <jiaminw@ymail.com>
2017-04-04 01:27:38 +0800

    Update Exception in CallCommandTest

--- test/sg/edu/nus/comp/cs4218/impl/cmd/CallCommandTest.java
+++ test/sg/edu/nus/comp/cs4218/impl/cmd/CallCommandTest.java
@@ -138,7 +138,7 @@ public class CallCommandTest {
 		assertEquals(cmdVector, resultStrVect);
 	}
 	
-	@Test (expected = NullPointerException.class)
+	@Test (expected = ShellException.class)
 	public void testGlobbing() throws AbstractApplicationException, ShellException{
 		inputLine = "cat article/*";
 		stdin = new ByteArrayInputStream(inputLine.getBytes());
