fea1515e1cfd1fd1434683a442eec8431ee82def
Jia Min <jiaminw@ymail.com>
2017-04-10 23:50:50 +0800

    Rebuttal Bug Fixed for #19

--- Hackathon/sg/edu/nus/comp/cs4218/ef1/GlobbingTest.java
+++ Hackathon/sg/edu/nus/comp/cs4218/ef1/GlobbingTest.java
@@ -16,12 +16,20 @@ import sg.edu.nus.comp.cs4218.impl.ShellImpl;
 
 public class GlobbingTest {
 
+	private static final String LINE_SEPARATOR = System
+			.getProperty("line.separator");
+	private static final String PATH_SEPARATOR = File.separator;
+	private static final String RELATIVE_DIR = "folder" + PATH_SEPARATOR
+			+ "SedAndWCFiles" + PATH_SEPARATOR + "sedTestFiles"
+			+ PATH_SEPARATOR;
+	private final ByteArrayOutputStream output = new ByteArrayOutputStream();
+	private String expected = "";
 	private static ShellImpl shell;
-	private String expected = "";;
 
 	@Before
 	public void setupBeforeTest() {
 		shell = new ShellImpl();
+		System.setOut(new PrintStream(output));
 	}
 
 	@Test
@@ -40,4 +48,27 @@ public class GlobbingTest {
 		String actual = shell.processGlob(cmdLine);
 		assertEquals(expected, actual);
 	}
+
+	@Test
+	public void testIntegrateGlobbingWithSedCat()
+			throws AbstractApplicationException, ShellException {
+		shell.parseAndEvaluate(
+				"sed s/sub/many/ sedIntegration > sedIntegration2.txt; "
+						+ "cat sedIntegration2.txt; cat " + RELATIVE_DIR + "*",
+				System.out);
+		String expected = "many sub string replacement substring substitution"
+				+ LINE_SEPARATOR + LINE_SEPARATOR + "0123456789"
+				+ LINE_SEPARATOR + "Hey, good to know <you>!" + LINE_SEPARATOR
+				+ "This is a small file consists of {1+1+0} lines."
+				+ LINE_SEPARATOR + "/* Hope this helps */ # no new line here"
+				+ LINE_SEPARATOR;
+
+		File file = new File("sedIntegration2.txt");
+		if (file.exists()) {
+			file.delete();
+		}
+
+		assertEquals(expected, output.toString());
+	}
+
 }
